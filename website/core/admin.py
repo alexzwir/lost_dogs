c7928357e7efb69a1ebaa339ac69583ff72e6d2d
alexzwir <alexzwir@gmail.com>
2018-11-04 11:27:03 -0200

    adding the models to be shown on admin section

--- website/core/admin.py
+++ website/core/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User,Person,Pet,LostPet
+from .models import User,Person,Pet,LostPet,CrewLostDog
 
+admin.site.register(CrewLostDog)
 admin.site.register(User)
 admin.site.register(Person)
 admin.site.register(Pet)
